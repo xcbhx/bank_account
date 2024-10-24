40b088a4a5448f18faf6baddd0c7e2fec83a3559
xchbx <ceina.ellison@gmail.com>
2024-10-24 13:21:15 -0400

    created class with attributes

---
+++ bank_account.py
@@ -0,0 +1,7 @@
+class BankAccount:
+    def __init__(self, full_name, account_number, balance=0):
+        self.name = full_name
+        self.account = account_number
+        self.balance = balance
+
+        
