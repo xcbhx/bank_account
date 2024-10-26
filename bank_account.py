f8bb88be7e350143a57e905d72f3ea05b4213c30
xchbx <ceina.ellison@gmail.com>
2024-10-26 10:46:36 -0400

    withdraw method

--- bank_account.py
+++ bank_account.py
@@ -4,14 +4,25 @@ import random
 class BankAccount:
     def __init__(self, full_name, account_number, balance=0):
         self.name = full_name
-        self.account = random.randint(10000000, 99999999)
+        self.account_number = random.randint(10000000, 99999999)
         self.balance = balance
 
     def deposit(self, amount):
         """Add a specified amount to the balance""" #Docstring
         # Update the balance by adding the amount
         self.balance += amount
-        print(f"Amount Deposited: ${amount} New Balance: ${self.balance}")
+        print(f'Amount Deposited: ${amount} New Balance: ${self.balance}')
+
+    def withdraw(self, amount):
+        """Withdraw a specified amount from the balance,w ith overdraft fees."""
+        if amount > self.balance:
+            # Apply the overdraft fee
+            self.balance -= (amount + 10)
+            print(f'Insufficient funds. You were charged an overdrafted fee of $10.\n Amount Withdrawn: ${amount} New Balance: ${self.balance}')
+        else: 
+            # Subtract the amount from the balance if sufficient funds
+            self.balance -= amount
+            print(f'Amount Withdrawn: ${amount} New Balance: ${self.balance}')
 
 
 
@@ -21,6 +32,7 @@ customer = BankAccount('Acs 1111', 20)
 
 print(f'Initial Balance: ${customer.balance}')
 
-# Call the deposit method the instance
+# Call the deposit method 
 customer.deposit(50) 
+customer.withdraw(60)
     
