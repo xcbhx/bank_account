ebcda155cb0d988afe69c3930275dc8534408939
xchbx <ceina.ellison@gmail.com>
2024-10-29 15:44:10 -0400

    fixed errors

--- bank_account.py
+++ bank_account.py
@@ -2,19 +2,19 @@
 import random
 
 class BankAccount:
-    def __init__(self, full_name, account_number, balance=0):
+    def __init__(self, full_name, account=None, balance=0):
         self.name = full_name
-        self.account = random.randint(10000000, 99999999)
+        self.account = account if account else random.randint(10000000, 99999999)
         self.balance = balance
 
     def deposit(self, amount):
-        """Add a specified amount to the balance""" #Docstring
+        """Add a specified amount to the balance""" # Docstring
         # Update the balance by adding the amount
         self.balance += amount
         print(f'Amount Deposited: ${amount} New Balance: ${self.balance}')
 
     def withdraw(self, amount):
-        """Withdraw a specified amount from the balance,w ith overdraft fees."""
+        """Withdraw a specified amount from the balance, with overdraft fees."""
         if amount > self.balance:
             # Apply the overdraft fee
             self.balance -= (amount + 10)
@@ -35,16 +35,23 @@ class BankAccount:
         self.balance += interest
         print(f'New balance with interest: ${self.balance}')
 
-    def print_statement(self):
+    def print_statement(self, account=None):
         """Display message with the account name, account number, and balance"""
-        print(f'{self.name}\nAccount No.:{self.account}\nBalance: ${self.balance}')
+        if account:
+            account_to_display = account # Use provided account number
+        else:
+            account_to_display = self.account # Use  default random account number
+
+            print(f'{self.name}\nAccount No.:{account_to_display}\nBalance: ${self.balance}')
 
 
 
 # Instantiate the class outside of the class definition
-customer = BankAccount('Acs 1111', 20)
+customer = BankAccount('Acs 1111')
+new_customer = BankAccount('Mitchell', '03141592')
 
 print(f'Initial Balance: ${customer.balance}')
+print(f'Initial Balance: ${new_customer.balance}')
 
 # Call the deposit method 
 customer.deposit(50) 
@@ -52,7 +59,14 @@ customer.withdraw(10)
 customer.add_interest()
 customer.print_statement()
 
-
-
 balance = customer.get_balance()
 print(f'The current balance is: ${balance}')
+
+new_customer.deposit(400000)
+new_customer.add_interest()
+new_customer.withdraw(150)
+
+new_balance = new_customer.get_balance()
+print(f'The current balance is: ${new_balance}')
+
+new_customer.print_statement()
